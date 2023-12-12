d0441080957d731b596497c339a60c83021fd7ab
itslakhvir <139687365+itslakhvir@users.noreply.github.com>
2023-12-12 19:15:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, request, render_template
+import model  # Assuming model.py contains necessary model functions
+import utils  # Assuming utils.py contains necessary utility functions
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        text = request.form['text']
+        # Assuming a function in model.py to make predictions
+        prediction = model.predict_stress(text)  
+        return render_template('result.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,111 @@
+# def create_label(row):
+#     if row['subreddit'] in ['ptsd', 'assistance', 'relationships', 'survivorsofabuse', 'domesticviolence']:
+#         if row['confidence'] >= 0.8:
+#             return 1  # Stress
+#         else:
+#             return 0  # No stress
+#     elif row['subreddit'] in ['anxiety', 'homeless', 'stress', 'almosthomeless', 'food_pantry']:
+#         if row['confidence'] >= 0.6:
+#             return 1  # Stress
+#         else:
+#             return 0  # No stress
+#     else:
+#         return 0  # Default to No stress
+# data['label'] = data.apply(create_label, axis=1)
+# print(data)
+
+# # Defining a function to create the label based on both the confidence score and the subreddit
+# def create_stress_label(row, confidence_threshold=0.8, stress_subreddits={'ptsd', 'anxiety', 'domesticviolence'}):
+#     # Label as stress if the confidence is above the threshold and the subreddit is one of the stress-related ones
+#     if row['confidence'] > confidence_threshold and row['subreddit'] in stress_subreddits:
+#         return 1
+#     else:
+#         return 0
+
+# # Apply the function to the dataset
+# data['label'] = data.apply(create_stress_label, axis=1)
+
+# # Display the first few rows with the new 'label' column
+# data.head()
+
+
+# Step 1: Text Preprocessing
+def wordopt(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub("\\W"," ",text) 
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)    
+    return text
+
+def remove_stopwords(text):
+    stpw = set(stopwords.words('english'))
+    filtered_text = [word for word in text if word not in stpw]
+    return filtered_text
+
+def lemmatize_words(text):
+    lemmer = WordNetLemmatizer()
+    lemmatized_text = [lemmer.lemmatize(word, pos='v') for word in text]
+    return lemmatized_text
+
+# Step 7: Classification Report and Confusion Matrix
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+# Function to plot confusion matrix
+def plot_confusion_matrix(y_true, y_pred, model_name):
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(6, 4))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False)
+    plt.title(f'Confusion Matrix for {model_name}')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.show()
+
+# Models and their predictions
+models = [RandomForestClassifier(), SVC(), KNeighborsClassifier()]
+model_names = ['RandomForestClassifier', 'SVC', 'KNeighborsClassifier']
+
+# Iterate through models and plot confusion matrix
+for model, model_name in zip(models, model_names):
+    pipeline = make_pipeline(TfidfVectorizer(), model)
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
+    
+    # Print classification report
+    print(f"Classification Report for {model_name}:\n{classification_report(y_test, y_pred)}")
+    
+    # Plot confusion matrix
+    plot_confusion_matrix(y_test, y_pred, model_name)
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Assuming you have the word_tokenize, remove_stopwords, and lemmatize_words functions defined
+def preprocess_text(text):
+    tokens = word_tokenize(text)
+    tokens = remove_stopwords(tokens)
+    tokens = lemmatize_words(tokens)
+    return ' '.join(tokens)
+
+# Split your data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=42)
+
+# Assuming you have a trained pipeline with SVC
+pipeline = make_pipeline(TfidfVectorizer(), SVC())
+pipeline.fit(X_train, y_train)
+
+# Assuming X_valid is your new data
+X_valid = input['text'].apply(preprocess_text)
+
+# Predict on the new data
+input['predicted'] = pipeline.predict(X_valid)
+input['predicted'] = input['predicted'].apply(lambda x: 'Stress' if x == 1 else 'Not Stress')
