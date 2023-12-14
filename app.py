cb81fc3c5ccfcfb000c5fe1a5d5895aabd6113da
itslakhvir <139687365+itslakhvir@users.noreply.github.com>
2023-12-14 22:07:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def predict():
         return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    
+    port = int(os.environ.get('PORT', 3000))
+    app.run(debug=True, host='0.0.0.0', port=port)
