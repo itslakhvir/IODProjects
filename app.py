3d6184bfd8ee5cc4ff9026e83555b5b57e6bb327
itslakhvir <139687365+itslakhvir@users.noreply.github.com>
2023-12-14 22:09:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
 
-from flask import Flask, request, render_template
-import model  # Assuming model.py contains necessary model functions
-import utils  # Assuming utils.py contains necessary utility functions
+from flask import Flask, request, jsonify
+import pickle
 import os
-import joblib
-import pandas as pd
+import logging
+from utils import preprocess_text  # assuming there's a preprocessing function in utils.py
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
-with open("stress_detection_model.pkl", "rb") as file:
-    model = joblib.load(file)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Load the pre-trained model
+model_path = os.path.join(os.getcwd(), 'stress_detection_model.pkl')
+try:
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+except FileNotFoundError:
+    logging.error("Model file not found.")
+    raise
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        text = request.form['text']
-        # Assuming a function in model.py to make predictions
-        prediction = model.predict(pd.Series(text))[0]  
-        return render_template('index.html', prediction=prediction)
+    try:
+        data = request.get_json(force=True)
+        text_input = data['text']
+        # Preprocess the text input if necessary
+        preprocessed_text = preprocess_text(text_input)
+
+        # Prediction
+        prediction = model.predict([preprocessed_text])[0]
+
+        return jsonify({"prediction": prediction}), 200
+    except KeyError:
+        return jsonify({"error": "Invalid input format. 'text' key is required."}), 400
+    except Exception as e:
+        logging.error(f"Error during prediction: {e}")
+        return jsonify({"error": "An error occurred during prediction."}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
