e28ca66a40d8d7a7836f7d51bd1d8e62525c54c6
Lakhvir <itslakhvir@gmail.com>
2023-12-14 20:26:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask, request, render_template
 #import model  # Assuming model.py contains necessary model functions
 #import utils  # Assuming utils.py contains necessary utility functions
 import os
+import joblib
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
+
+with open("stress_detection_model.pkl", "rb") as file:
+    model = joblib.load(file)
+
 
 @app.route('/')
 def home():
@@ -15,11 +22,11 @@ def predict():
     if request.method == 'POST':
         text = request.form['text']
         # Assuming a function in model.py to make predictions
-        #prediction = model.predict_stress(text)  
-        prediction="stressed"
+        prediction = model.predict(pd.Series(text))[0]  
+        prediction=np.where(prediction == 1, "Stressed", "Not Stressed")
         return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(debug=True, host='0.0.0.0', port=port)
     
