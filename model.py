37f29dbc43cfae691a83036a41c84172650a7742
itslakhvir <139687365+itslakhvir@users.noreply.github.com>
2023-12-15 09:15:34 +0800

    Update model.py

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
 
-data=pd.read_csv(r"C:\Users\itsla\Downloads\CapstoneProject\Deploymentfiles\Stress.csv")
+data=pd.read_csv(r"Stress.csv")
 
 # Split your data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=42)
